04edfaa8be16b5463692814a8771d87286a10a9d
felipemello987 <felipemello200518@gmail.com>
2026-04-24 13:16:30 -0300

    commit4

--- src/codigo_com_erros.c
+++ src/codigo_com_erros.c
@@ -3,44 +3,52 @@
 
 int main() {
     float nota1, nota2, media;
-    int opcao, total, faltas;
+    int opcao, total, faltas, continuar;
 
-    printf("========== SISTEMA ACADEMICO ==========\n");
-    printf("Oque deseja fazer?\n");
-    printf("Opcao 1: Calcular media do aluno\nOpcao 2: Calcular presenca do aluno\n");
-    scanf("%d", &opcao);
+ continuar = 1;
+
+    while (continuar) {
+        printf("========== SISTEMA ACADEMICO ==========\n");
+        printf("Oque deseja fazer?\n");
+        printf("Opcao 1: Calcular media do aluno\nOpcao 2: Calcular presenca do aluno\nOpcao 3: Sair\n");
+        scanf("%d", &opcao);
 
 
-    switch(opcao) {
-        case 1: {
-        printf("Digite a primeira nota: ");
-        scanf("%f", &nota1);
+        switch(opcao) {
+            case 1: {
+            printf("Digite a primeira nota: ");
+            scanf("%f", &nota1);
 
-        printf("Digite a segunda nota: ");
-        scanf("%f", &nota2);
+            printf("Digite a segunda nota: ");
+            scanf("%f", &nota2);
 
-        media = nota1 + nota2 / 2;
+            media = nota1 + nota2 / 2;
 
-        if (media >= 6) {
-            printf("Aluno aprovado\n");
-        } else {
-            printf("Aluno reprovado\n");
-        }
-        break;
-        }
-        case 2: {
-            printf("Digite a quantidade total de aulas: ");
-            scanf("%d", &total);
-            printf("Digite quantas aulas o aluno faltou: ");
-            scanf("%d", &faltas);
-
-            if (faltas >= (total*0.25)) {
-                printf("Reprovado por faltas");
+            if (media >= 6) {
+                printf("Aluno aprovado\n");
             } else {
-                printf("Presenca dentro do esperado");
+                printf("Aluno reprovado\n");
             }
-        break;    
+            break;
+            }
+            case 2: {
+                printf("Digite a quantidade total de aulas: ");
+                scanf("%d", &total);
+                printf("Digite quantas aulas o aluno faltou: ");
+                scanf("%d", &faltas);
+
+                if (faltas >= (total*0.25)) {
+                    printf("Reprovado por faltas\n");
+                } else {
+                    printf("Presenca dentro do esperado\n");
+                }
+            break;    
+            }
+            case 3: {
+                continuar = 0;
+            }
         }
+        
     }
     return 0;
 }
